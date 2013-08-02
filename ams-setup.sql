ff3df97d746c341e0675e0d29ef33c447e6e026b
Risa Newyear-Ramirez <risapisa@gmail.com>
2013-08-02 16:20:31 -0700

    Fixed some of the bugs, still won't build LeadSinger

--- ams-setup.sql
+++ ams-setup.sql
@@ -1,45 +1,48 @@
 create table Item
 	(upc integer not null PRIMARY KEY,
-	title varchar2(20),
-	type varchar2(10),
-	category varchar2(10),
-	company varchar2(20),
+	title varchar(20),
+	type varchar(10),
+	category varchar(10),
+	company varchar(20),
 	year integer,
 	price integer,
 	stock integer);
 
 create table LeadSinger
-	(upc integer not null PRIMARY KEY,
-	name varchar2(20) not null PRIMARY KEY,
-	Foreign Key (LeadSinger_upc) REFERENCES Item);
+	(upc integer not null,
+	name varchar(20) not null,
+    PRIMARY KEY (upc, name),
+	Foreign Key (upc) REFERENCES Item);
 
 create table HasSong
-	(upc integer not null PRIMARY KEY,
-	title varchar2(15) PRIMARY KEY
+	(upc integer not null,
+	title varchar(15) not null,
 	PRIMARY KEY (upc, title),
 	Foreign Key (upc) references Item);
 
 create table Purchase
 	(receiptId integer not null PRIMARY KEY,
 	pdate integer,
-	cid integer FOREIGN KEY REFERENCES Customer,
+	cid integer ,
 	cardnum integer,
 	expiryDate integer,
 	expectedDate integer,
-	deliveredDate integer);
+	deliveredDate integer,
+    Foreign Key (cid) REFERENCES Customer));
 
 create table PurchaseItem
-	(receiptId integer not null PRIMARY KEY,
-	upc integer not null PRIMARY KEY,
+	(receiptId integer not null,
+	upc integer not null,
 	quantity integer
+    PRIMARY KEY (receiptId, upc),
 	Foreign Key (receiptId) REFERENCES Purchase
 	Foreign Key (upc) REFERENCES Item);
 
 create table Customer
 	(cid integer not null,
 	password varchar(8),
-	name varchar2(20),
-	address varchar2(20),
+	name varchar(20),
+	address varchar(20),
 	phone integer,
 	PRIMARY KEY (cid));
 
@@ -56,6 +59,3 @@ create table ReturnItem
 	quantity integer,
 	PRIMARY KEY (retid, upc),
 	FOREIGN KEY (upc) REFERENCES Item);
-
-
-
